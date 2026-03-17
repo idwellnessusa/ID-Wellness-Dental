b896486503b4fa16d329cea03b51d7b6e6010ed3
idwellnessusa <idwellnessusa@gmail.com>
2026-03-17 12:06:18 -0400

    debug: check api keys

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,3 +1,4 @@
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://majqwqxtdtjyotforfml.supabase.co'
